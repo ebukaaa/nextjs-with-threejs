682d268781d5347d86107438667bf169bac729ea
Ebuks <ebuks@Ebuks-Mac.local>
2020-09-14 22:33:25 +0100

    v1.0.2 => added 3D objects

--- pages/index.page.jsx
+++ pages/index.page.jsx
@@ -1,9 +1,36 @@
-import { useStore } from "./utils";
+import { use3DStore } from "./utils";
 
-export const useHome = () => {
-  const { styles } = useStore();
+export const use3D = () => {
+  const {
+    styles,
+    Boxes,
+    Canvas,
+    Lights,
+    Floor,
+    Suspense,
+    OrbitControls,
+  } = use3DStore();
 
-  return <main className={styles.home} />;
+  return (
+    <main className={styles.home}>
+      <Canvas
+        shadowMap
+        colorManagement
+        camera={{ position: [-5, 2, 10], fov: 60 }}
+      >
+        <Lights />
+        <>
+          <Suspense fallback={null}>
+            <group>
+              <Boxes />
+              <Floor />
+            </group>
+          </Suspense>
+        </>
+        <OrbitControls />
+      </Canvas>
+    </main>
+  );
 };
 
-export default useHome;
+export default use3D;
--- pages/utils/index.js
+++ pages/utils/index.js
@@ -1,12 +1,87 @@
+import { createRef, Suspense } from "react";
+import { useFrame, Canvas } from "react-three-fiber";
+import { string, arrayOf, oneOfType, number } from "prop-types";
+import { MeshWobbleMaterial, softShadows, OrbitControls } from "drei";
 import styles from "./style.module.scss";
 
-function get({ prop }) {
-  return prop;
-}
+softShadows();
 
-export const useStore = () => {
+function Boxes() {
+  return (
+    <>
+      <Box position={[0, 1, 0]} args={[3, 2, 1]} color="lightblue" speed={2} />
+      <Box position={[-2, 1, -5]} color="pink" speed={6} />
+      <Box position={[5, 1, -2]} color="pink" speed={6} />
+    </>
+  );
+}
+function Floor() {
+  return (
+    <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -3, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <shadowMaterial attach="material" opacity={0.3} />
+    </mesh>
+  );
+}
+function Lights() {
+  return (
+    <>
+      <ambientLight intensity={0.3} />
+      <directionalLight
+        castShadow
+        shadow-mapSize-height={1024}
+        shadow-mapSize-width={1024}
+        shadow-camera-far={50}
+        shadow-camera-top={10}
+        shadow-camera-right={10}
+        shadow-camera-left={-10}
+        shadow-camera-bottom={-10}
+        position={[0, 10, 0]}
+        intensity={1.5}
+      />
+      <pointLight position={[-10, 0, -20]} intensity={0.5} />
+      <pointLight position={[0, -10, 0]} intensity={1.5} />
+    </>
+  );
+}
+function Box({ position, args, color, speed }) {
+  const mesh = createRef();
+  const spin = () => {
+    mesh.current.rotation.y += 0.01;
+    mesh.current.rotation.x = mesh.current.rotation.y;
+  };
+  useFrame(spin);
+
+  return (
+    <mesh castShadow ref={mesh} position={position}>
+      <boxBufferGeometry attach="geometry" args={args} />
+      <MeshWobbleMaterial
+        attach="material"
+        color={color}
+        speed={speed}
+        factor={0.6}
+      />
+    </mesh>
+  );
+}
+Box.propTypes = {
+  position: arrayOf(oneOfType([number])).isRequired,
+  args: arrayOf(oneOfType([number])),
+  color: string.isRequired,
+  speed: number.isRequired,
+};
+Box.defaultProps = {
+  args: [1, 1, 1],
+};
+
+export const use3DStore = () => {
   return {
-    get: get({}),
+    Boxes,
+    Lights,
+    Floor,
+    OrbitControls,
+    Suspense,
+    Canvas,
     styles,
   };
 };
